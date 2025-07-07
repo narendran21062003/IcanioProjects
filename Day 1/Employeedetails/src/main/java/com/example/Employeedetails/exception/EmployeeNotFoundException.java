9ac50f72d34d8a77a9cfc7a8c65d4832be0b5563
narendran21062003 <narendranm2106@gmail.com>
2025-07-07 21:33:55 +0530

    file deleted

--- Day 1/Employeedetails/src/main/java/com/example/Employeedetails/exception/EmployeeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.Employeedetails.exception;
-
-// Custom exception for employee not found
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(String message) {
-        super(message);
-    }
-}
